4fd5acce62d83a4654a2e93ac1c4840e3d5b2f75
ahrorsoliev <akhsoliev@gmail.com>
2023-03-10 10:42:01 +0500

    protected route, login, logout added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,41 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useAuthContext } from "../hooks/useAuthContext";
+import { useLogout } from "../hooks/useLogout";
 
 function Navbar() {
+  const { user } = useAuthContext();
+  const { logout } = useLogout()
   return (
-    <div className='bg-green-300 p-5 mb-10'>
-        <nav className='flex justify-between items-center max-w-5xl mx-auto'>
-            <Link to="/">
-                <h1 className="font-bold text-2xl">myMoney</h1>
-            </Link>
+    <div className="bg-green-300 p-5 mb-10">
+      <nav className="flex justify-between items-center max-w-5xl mx-auto">
+        <Link to="/">
+          <h1 className="font-bold text-2xl">myMoney</h1>
+        </Link>
 
-            <ul className='flex gap-5 text-base'>
+        <ul className="flex items-center gap-5 text-base">
+          {!user && (
+            <>
               <li>
                 <Link to="/login">Login</Link>
               </li>
               <li>
                 <Link to="/signup">Sing up</Link>
               </li>
-            </ul>
-        </nav>
+            </>
+          )}
+          {user && (
+            <>
+            <p>hello, {user.displayName}</p>
+            <button onClick={logout} className="border-2 px-2 py-1 rounded border-red-400 hover:bg-red-400 hover:text-white text-base">
+              Logout
+            </button>
+            </>
+          )}
+        </ul>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
+import { useLogin } from '../../hooks/useLogin'
 
 function Login() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const {error, isPending, login} = useLogin()
 
   const hanldeSubmit = (e) => {
     e.preventDefault()
-    console.log(
+    login(
       email,
       password
     )
@@ -23,7 +25,9 @@ function Login() {
           <span className='block text-base'>password:</span>
           <input onChange={(e) => {setPassword(e.target.value)}} placeholder="password" type="password" className='placeholder:text-base border-2 border-l-neutral-300 rounded focus:border-2 focus:border-green-300 focus:outline-none px-2 py-1' />
         </label>
-        <button type='submit' className='border-2 px-2 py-1 rounded border-green-400 hover:bg-green-400 hover:text-white text-base'>Login</button>
+        {!isPending && <button type='submit' className='border-2 px-2 py-1 rounded border-green-400 hover:bg-green-400 hover:text-white text-base'>Login</button>}
+        {isPending && <button type='submit' className='border-2 px-2 py-1 rounded border-green-400 hover:bg-green-400 hover:text-white text-base' disabled>Loading...</button>}
+        {error && <p>{error}</p>}
       </form>
     </div>
   )
